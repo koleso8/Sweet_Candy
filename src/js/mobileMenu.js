d640b2f6749fc680353c4d657608fd95c5f3e538
koleso8 <dev.koleso@gmail.com>
2024-09-13 18:07:16 +0300

    fix menu

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -2,5 +2,11 @@ const burgerNav = document.querySelector('.burgerNav');
 const close = document.querySelector('.mobile_menu_close');
 const menuClass = document.querySelector('.mobile_menu_box');
 
-burgerNav.addEventListener('click', () => menuClass.classList.toggle('active'));
-close.addEventListener('click', () => menuClass.classList.toggle('active'));
+burgerNav.addEventListener('click', () => {
+  menuClass.classList.toggle('active');
+  document.body.classList.add('modal-open');
+});
+close.addEventListener('click', () => {
+  document.body.classList.remove('modal-open');
+  menuClass.classList.toggle('active');
+});
